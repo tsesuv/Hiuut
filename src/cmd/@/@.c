efcdd08b61d76ee286a014868c46f0563c5b1b8d
tsesuv <tsesuv08@gmail.com>
2026-05-17 13:46:29 +0900

    またコンパイラの作り直し...何回やり直したいんだ() 	modified:   ../../../reff/@/case0.@ 	modified:   ../../../reff/@/intro.@ 	modified:   @ 	modified:   @.c 	modified:   Include/token.h

--- src/cmd/@/@.c
+++ src/cmd/@/@.c
@@ -1,22 +1,57 @@
 /* UnSynk @ Compiler */
-/* Build: 20260505XXXX */
+/* Build: 20260517XXXX */
 /* Created by UnSynk, tsesuv notsel */
 
 #include <stdio.h>
-#include "str.h"
+#include <stdlib.h>
+#include "Include/token.h"
 
 FILE *infile;
 
-bool isSpc(char c);
+bit cmp(byte a, byte b);
+uint len(byte *p);
 
 int main(int ac, char **av)
-{	int i = 0;
-	str s = strset("Hello, world!");
-	txoutln(s);
+{	if(ac == 1)
+	{	return 0;
+	}
+
+	uint l = 0;
+	for(uint k = 0; k < ac; k++)
+	{	for(uint i = 0; i < len(av[k]); i++) l++;
+	}
+
+	byte *ag = (byte *)malloc((l + ac) * sizeof(byte));
+
+	l = 0;
+	for(uint k = 0; k < ac; k++)
+	{	for(uint i = 0; i < len(av[k]); i++)
+		{	ag[l++] = *(av[k] + i);
+		}
+		ag[l++] = 0;
+	}
+
+	printf("Length: %d\n", l);
+
+	for(uint i = 0; i < l; i++) printf("%c ($%02X)\n", ag[i], ag[i]);
+
+	free(ag);
 
 	return 0;
 }
 
-bool isSpc(char c)
-{	return (c == ' ');
+bit cmp(byte a, byte b)
+{	bit x;
+
+	x.f = (a == b);
+
+	return x;
+}
+
+uint len(byte *p)
+{	byte *t = p;
+
+	while(*p++);
+
+	return p - t - 1;
 }
